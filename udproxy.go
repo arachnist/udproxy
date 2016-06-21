7ddff50b2fae6486df598ad8d98be03de880f110
Robert S. Gerus <ar@bash.org.pl>
2016-06-21 23:36:42 +0200

    We now write our own configuration and react to SIGINT

--- udproxy.go
+++ udproxy.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"time"
+	"os/signal"
 )
 
 func checkErr(err error) {
@@ -17,15 +16,20 @@ func checkErr(err error) {
 }
 
 type Backend struct {
-	Upstream string        `json:"backend"`
-	Local    string        `json:"local"`
-	input    chan []byte   `json:"input"`
-	quit     chan struct{} `json:"quit"`
+	Upstream string `json:"backend"`
+	Local    string `json:"local"`
+	input    chan []byte
+	quit     chan struct{}
+}
+
+type Listener struct {
+	Address string `json:"address"`
+	quit    chan struct{}
 }
 
 type udproxyConfig struct {
 	Backends map[string]Backend `json:"backends,inline"`
-	Sockets  []string           `json:"sockets"`
+	Listen   []Listener         `json:"listen"`
 	Clients  map[string]string  `json:"clients"`
 }
 
@@ -41,7 +45,7 @@ func backend(local, remote string, quit chan struct{}, input chan []byte) {
 
 	defer conn.Close()
 
-	log.Println("Ready for action", local, remote)
+	log.Println("Backend connection ready for action", local, remote)
 
 	for {
 		select {
@@ -65,6 +69,7 @@ func spawnBackend(local, remote string) (chan struct{}, chan []byte) {
 
 func main() {
 	var config udproxyConfig
+	quit := make(chan struct{}, 1)
 
 	if len(os.Args) < 2 {
 		log.Fatalln("Usage:", os.Args[0], "<config file>")
@@ -76,6 +81,10 @@ func main() {
 	err = yaml.Unmarshal(data, &config)
 	checkErr(err)
 
+	data, err = yaml.Marshal(config)
+	checkErr(err)
+	log.Print("Parsed configuration:\n", string(data))
+
 	for name, backend := range config.Backends {
 		quit, input := spawnBackend(backend.Local, backend.Upstream)
 		config.Backends[name] = Backend{
@@ -86,8 +95,27 @@ func main() {
 		}
 	}
 
-	data, err = yaml.Marshal(config)
-	fmt.Println(string(data))
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
 
-	time.Sleep(90 * time.Second)
+		<-c
+		log.Println("Shutting down!")
+		for name, backend := range config.Backends {
+			log.Println("Stopping backend", name)
+			backend.quit <- struct{}{}
+		}
+
+		log.Println("Writing config!")
+		data, err = yaml.Marshal(config)
+		checkErr(err)
+
+		err = ioutil.WriteFile(os.Args[1], data, 0600)
+		checkErr(err)
+
+		log.Println("Finally stopping!")
+		quit <- struct{}{}
+	}()
+
+	<-quit
 }
